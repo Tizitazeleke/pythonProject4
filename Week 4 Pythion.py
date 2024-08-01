bdaf1d1dcb644d03126e89001b2baef108339086
Tizitazeleke <126310280+Tizitazeleke@users.noreply.github.com>
2024-07-31 18:16:44 -0700

    Week 2 assignment

---
+++ Week 4 Pythion.py
@@ -0,0 +1,10 @@
+# Define the list
+istA = [23, 12, 45, 22, 11, 78, 13, 90, 45, 88, 10, -5, -1]
+
+# Find the maximum and minimum numbers
+max_number = max(istA)
+min_number = min(istA)
+
+# Print the results
+print("Maximum number:", max_number)
+print("Minimum number:", min_number)
